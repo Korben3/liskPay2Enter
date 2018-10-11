15ea42ae689643b7a6f0814e098f0b9e4d1dbb2f
Korben3 <42899281+Korben3@users.noreply.github.com>
2018-10-11 23:28:39 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 // --------------------------------------------------------------------------------
 //
 $title="Lisk Pay 2 Enter - key in reference - Hot Cars"; //website title
-$costToEnter=2;		//cost to access the website in lisk
+$costToEnter=2;	//cost to access the website in lisk
 $totalTX=15;		//number of last transactions to check, increase if your site is popular
 $keyStrenght=8;	//max 32, higher is safer but more expensive (more bytes are used in the tx)
 $nodeUrl="https://testnet.lisk.io";	//node url, use a private/dedicated one to maximize performance
